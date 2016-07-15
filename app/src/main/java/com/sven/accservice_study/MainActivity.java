3657661e3f988c2537a2cd9ade412c9234edca9a
Sven <783178690@qq.com>
2016-07-15 17:28:20 +0800

    自动化实现一半   模拟在edittext中输入内容返回后会报null

--- app/src/main/java/com/sven/accservice_study/MainActivity.java
+++ app/src/main/java/com/sven/accservice_study/MainActivity.java
@@ -1,98 +1,32 @@
 package com.sven.accservice_study;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
 import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    private static Handler messageHandler;
+
+    @Bind(R.id.button1)
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button1);
-//        simulate(button);
-
-
-        Looper looper = Looper.myLooper();
-        messageHandler = new MessageHandler(looper);
-        //此处的作用是延迟1秒，然后激活点击事件
-        //欢迎转载并说明转自：http://blog.csdn.net/aminfo/article/details/7887964
-        new Thread() {
-            @Override
-            public void run() {
-                synchronized (this) {
-                    try {
-                        wait(1000); //1秒
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-                Message message = Message.obtain();
-                message.what = 1;
-                messageHandler.sendMessage(message);
-            }
-        }.start();
+        ButterKnife.bind(this);
     }
 
-    class MessageHandler extends Handler {
-        public MessageHandler(Looper looper) {
-            super(looper);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            Bundle bundle = msg.getData();
-            switch (msg.what) {
-                case 1:
-                    //模拟点击按钮
-                    button.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, button.getLeft() + 5, button.getTop() + 5, 0));
-                    button.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, button.getLeft() + 5, button.getTop() + 5, 0));
-
-                    //以下代码模拟点击文本编辑框
-                    //et.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, et.getLeft()+5, et.getTop()+5, 0));
-                    //et.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, et.getLeft()+5, et.getTop()+5, 0));
-                    break;
-                default:
-                    break;
-            }
-
-        }
+    @OnClick(R.id.button1)
+    public void onClick() {
+        Intent intent = new Intent(
+                android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
     }
-
-  /*  public void clickeMe() {
-        Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
-    }
-
-    public void simulate(View view) {
-        setSimulateClick(button, 50f, 50f);
-    }
-
-    private void setSimulateClick(View view, float x, float y) {
-        for (int i = 0; i < 1000; i++) {
-            long downTime = SystemClock.uptimeMillis();
-            final MotionEvent downEvent = MotionEvent.obtain(downTime, downTime,
-                    MotionEvent.ACTION_DOWN, x, y, 0);
-            downTime += 1000;
-            final MotionEvent upEvent = MotionEvent.obtain(downTime, downTime,
-                    MotionEvent.ACTION_UP, x, y, 0);
-            view.onTouchEvent(downEvent);
-            view.onTouchEvent(upEvent);
-            downEvent.recycle();
-            upEvent.recycle();
-            clickeMe();
-        }
-    }
-*/
-
 }
 
 
--- app/src/main/java/com/sven/accservice_study/MyAccessibility.java
+++ app/src/main/java/com/sven/accservice_study/MyAccessibility.java
@@ -2,10 +2,10 @@ package com.sven.accservice_study;
 
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
-import android.os.SystemClock;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
@@ -13,183 +13,283 @@ import java.util.List;
 
 /**
  * Created by Sven on 2016/7/14 0014.
- *
  */
 
 public class MyAccessibility extends AccessibilityService {
 
     private static final String TAG = "MyAccessibility";
-    String[] PACKAGES = {"com.tencent.mm"};
+    //    String[] PACKAGES = {"com.tencent.mm"};
+    String[] PACKAGES = {"com.sven.accservice_test"};
 
     @Override
     protected void onServiceConnected() {
         Log.e(TAG, "config success!");
-       /* AccessibilityServiceInfo accessibilityServiceInfo = new AccessibilityServiceInfo();
+        /*辅助服务要观察的app包名以及事件类型的配置*/
+        /*AccessibilityServiceInfo accessibilityServiceInfo = new AccessibilityServiceInfo();
         accessibilityServiceInfo.packageNames = PACKAGES;
         accessibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
         accessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
         accessibilityServiceInfo.notificationTimeout = 1000;
         setServiceInfo(accessibilityServiceInfo);*/
-
-
     }
 
 
     /*该服务类继承了3个方法：
      1.onServiceConnected()：绑定服务所用方法，一些初始化的操作放在这里面。
-     2.onAccessibilityEvent(AccessibilityEvent event)：响应AccessibilityEvent的事件，在用户操作的过程中，系统不断的发送sendAccessibiltyEvent(AccessibilityEvent event)；然后通过onAccessibilityEvent()可以捕捉到该事件，然后分析。
-     3.public void onInterrupt()：打断获取事件的过程，本例中暂不适用。
-    在onServiceConnected()我们做了一些初始化的工作，通过AccessibilityServiceInfo设置了AccessibilityService的一些参数
-    //ccessibilityServiceInfo.packageNames = PACKAGES：响应某个应用的时间，包名为应用的包名；可以用String[]对象传入多包。如果不设置，默认响应所有应用的事件。
-    ssibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK：响应时间的类型，事件分很多种：单击、长按、滑动等，需要指定，我设置了所有事件都响应：TYPES_ALL_MASK。
-    cessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN：设置回馈给用户的方式，是语音播出还是振动。这个我们稍后尝试配置一些TTS引擎，让它发音。
-    cessibilityServiceInfo.notificationTimeout = 1000：看意思就能明白，响应时间的设置。*/
 
+     2.onAccessibilityEvent(AccessibilityEvent event)：响应AccessibilityEvent的事件，在用户操作的过程
+     中，系统不断的发送sendAccessibiltyEvent(AccessibilityEvent event)；然后通过onAccessibilityEvent()
+     可以捕捉到该事件，然后分析。
+
+     3.public void onInterrupt()：打断获取事件的过程，本例中暂不适用。
+     在onServiceConnected()我们做了一些初始化的工作，通过AccessibilityServiceInfo设置了AccessibilityService的一些参数
+     //ccessibilityServiceInfo.packageNames = PACKAGES：响应某个应用的时间，包名为应用的包名；可以用String[]
+     对象传入多包。如果不设置，默认响应所有应用的事件。
+
+     ssibilityServiceInfo.eventTypes = AccessibilityEvent.TYPES_ALL_MASK：响应时间的类型，事件分很多种：
+     单击、长按、滑动等，需要指定，我设置了所有事件都响应：TYPES_ALL_MASK。
+
+     cessibilityServiceInfo.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN：设置回馈给用户的方式，
+     是语音播出还是振动。这个我们稍后尝试配置一些TTS引擎，让它发音。
+
+     cessibilityServiceInfo.notificationTimeout = 1000：看意思就能明白，响应时间的设置。*/
+
+    /*这些设置很重要，因为AccessibilityService服务不是用户去开启的。我们无法用startService()方法启动它，
+    所以我连activity都没写。因为它是通过该配置让系统自动识别的，识别成功后，就会在我们手机里面看到该服务，
+    然后需要手动开启该服务*/
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG, "accessibilityEvent.getPackageName():" + event.getPackageName());
-        Log.e(TAG, "accessibilityEvent.getClassName():" + event.getClassName());
+
+
         int eventType = event.getEventType();
-        String className= (String) event.getClassName();
+        String packName = (String) event.getPackageName();
+        String className = (String) event.getClassName();
+        Log.e(TAG, "__________start____________");
+        Log.e(TAG, "accessibilityEvent.getPackageName():" + packName);
+        Log.e(TAG, "accessibilityEvent.getClassName():" + className);
 
         String eventText = "";
         switch (eventType) {
+
+            /*在调用这个getSource()方法时，需要事先配置一下，也就是需要android：canRetrieveWindowContent="true"，
+            这个属性是API14（android4.0）给出的，它的配置方法受限于需要采用XML初始化的方式配置AccessibilityService，
+            也就是我们现在不在onServiceConnected()里配置初始化信息，改在xml里实现。那么我们现在就要将上面的程序修改一下。
+            首先，删除onServiceConnected()的配置(因为getSource()不是所有的操作都有该方法，所以得在特定的方法中实现该方法，
+            我们选择在TYPE_VIEW_CLICKED里实现)*/
+
+            /*view被点击*/
             case AccessibilityEvent.TYPE_VIEW_CLICKED:
-                /*获取被点击的控件信息*/
+                eventText = "TYPE_VIEW_CLICKED";
+                /*获得控件信息*/
                 AccessibilityNodeInfo noteInfo = event.getSource();
-                if (noteInfo.getClassName()!=null&&noteInfo.getClassName().equals("android.widget.TextView")&&noteInfo.getPackageName().equals("com.tencent.mm")) {
-                    openPacket();
-                }
-                
-                
-                
-                
-                Log.e(TAG, noteInfo.toString());
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
                 break;
+            /*view获取到焦点*/
             case AccessibilityEvent.TYPE_VIEW_FOCUSED:
                 eventText = "TYPE_VIEW_FOCUSED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
                 break;
+            /*view被长按*/
             case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
                 eventText = "TYPE_VIEW_LONG_CLICKED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
                 break;
+            /*view被选择*/
             case AccessibilityEvent.TYPE_VIEW_SELECTED:
                 eventText = "TYPE_VIEW_SELECTED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
+            /*view的文本发生变化*/
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
                 eventText = "TYPE_VIEW_TEXT_CHANGED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
+            /*窗口状态改变*/
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
                 eventText = "TYPE_WINDOW_STATE_CHANGED";
-
-                /*如果进入了启动页面*/
+                /*如果在首页*/
                 if (className.equals("com.tencent.mm.ui.LauncherUI")) {
-
+                    jump();
                 }
-
+                /*如果是搜索主页*/
+                else if (className.equals("com.tencent.mm.plugin.search.ui.FTSMainUI")) {
+                    jump2();
+                }
+                /*如果是二级文章搜索页面*/
+                else if (className.equals("com.tencent.mm.plugin.webview.ui.tools.fts.FTSSearchTabWebViewUI")) {
+                   /* jump2();*/
+                    jump3();
+                }
                 break;
+            /*通知栏状态改变*/
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
                 eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
                 eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_ANNOUNCEMENT:
                 eventText = "TYPE_ANNOUNCEMENT";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
                 eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
                 eventText = "TYPE_VIEW_HOVER_ENTER";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
                 eventText = "TYPE_VIEW_HOVER_EXIT";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_VIEW_SCROLLED:
                 eventText = "TYPE_VIEW_SCROLLED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
             case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
                 eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
+
                 break;
+            /*窗口的内容发生变化*/
             case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
                 eventText = "TYPE_WINDOW_CONTENT_CHANGED";
-                break;
-            default:
-                eventText = "default";
+                noteInfo = event.getSource();
+                Log.e(TAG, noteInfo != null ? noteInfo.toString() : "为空");
                 break;
         }
-        Log.e(TAG, eventText);
+        Log.e(TAG, "事件类型:     " + eventText);
+        Log.e(TAG, "__________end____________");
+    }
+
+    /*微信文章搜索页面*/
+    /*com.tencent.mm.plugin.webview.ui.tools.fts.FTSSearchTabWebViewUI*/
+
+    /*微信搜索主页面*/
+    /*com.tencent.mm.plugin.search.ui.FTSMainUI*/
+
+    @SuppressLint("NewApi")
+    private void jump() {
+        AccessibilityNodeInfo noteInfo = getRootInActiveWindow();
+        if (noteInfo != null) {
+
+           /* if (noteInfo.getContentDescription().equals("搜索")) {
+                Log.e(TAG, "搜索");
+            }*/
+
+            Log.e(TAG, "noteInfo.getContentDescription():" + noteInfo.getContentDescription());
+            Log.e(TAG, "noteInfo.getContentDescription():" + noteInfo.getClassName());
+
+            List<AccessibilityNodeInfo> list = noteInfo
+                    .findAccessibilityNodeInfosByText("");
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(0).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(2).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(3).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(4).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(5).getChild(1).getContentDescription());
+            Log.e(TAG, "list.size():" + list.size() + "");
+
+            noteInfo.getChild(5).getChild(1).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
     }
 
     @SuppressLint("NewApi")
-    private void openPacket() {
-        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-        if (nodeInfo != null) {
-            List<AccessibilityNodeInfo> list = nodeInfo
-                    .findAccessibilityNodeInfosByText("搜索");
+    private void jump2() {
+        AccessibilityNodeInfo noteInfo = getRootInActiveWindow();
+        if (noteInfo != null) {
+            /*List<AccessibilityNodeInfo> list = noteInfo
+                    .findAccessibilityNodeInfosByText("2");
             for (AccessibilityNodeInfo n : list) {
                 n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
-        }
+            }*/
 
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(0).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(2).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(3).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(4).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(5).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(6).getClassName());
+
+            noteInfo.getChild(3).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        }
     }
+
 
     @SuppressLint("NewApi")
-    private void getPacket() {
+    private void jump3() {
+        AccessibilityNodeInfo noteInfo = getRootInActiveWindow();
+        if (noteInfo != null) {
+            List<AccessibilityNodeInfo> list = noteInfo
+                    .findAccessibilityNodeInfosByText("搜索文章");
 
-        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-        if (rootNode != null) {
-            List<AccessibilityNodeInfo> nodeInfos = rootNode
-                    .findAccessibilityNodeInfosByText("领取红包");
-            for (AccessibilityNodeInfo nodeInfo : nodeInfos) {
-                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+          /*  Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(0).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getClassName());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(0).getChildCount());
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChildCount());
+
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChild(0));
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChild(1));
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChild(2));
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChild(3));
+            Log.e(TAG, "noteInfo.getChildCount():" + noteInfo.getChild(1).getChild(4));*/
+
+
+            Log.e(TAG, "list.size():" + list.size());
+
+//            Log.e(TAG, "noteInfo.getChild(1).getChild(2).getClassName():" + noteInfo.getChild(1).getChild(2).getClassName());
+
+            Bundle bundle = new Bundle();
+            bundle.putString("bundle", "Nihao");
+
+            Bundle arguments = new Bundle();
+            arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
+                    "你好");
+            Log.e(TAG, "noteInfo.isVisibleToUser():" + noteInfo.isVisibleToUser());
+
+            if (noteInfo.isVisibleToUser()) {
+                noteInfo.getChild(1).getChild(2).performAction(AccessibilityNodeInfo.AccessibilityAction.ACTION_SET_TEXT.getId(), arguments);
             }
+
+//            noteInfo.getChild(1).getChild(2).setText("你好");
+           /* for (AccessibilityNodeInfo n : list) {
+                n.performAction(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE);
+            }*/
         }
     }
 
+    /*当服务事件被中断时*/
     @Override
     public void onInterrupt() {
 
-        /*int eventType = event.getEventType();
-        switch (eventType) {
-            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:// 通知栏事件
-                List<CharSequence> texts = event.getText();
-                if (!texts.isEmpty()) {
-                    for (CharSequence text : texts) {
-                        String content = text.toString();
-                        if (content.contains("[微信红包]")) {
-                            // 监听到微信红包的notification，打开通知
-                            if (event.getParcelableData() != null
-                                    && event.getParcelableData() instanceof Notification) {
-                                Notification notification = (Notification) event
-                                        .getParcelableData();
-                                PendingIntent pendingIntent = notification.contentIntent;
-                                try {
-                                    pendingIntent.send();
-                                } catch (CanceledException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    }
-                }
-
-                break;*/
-
-//        }
-
-    }
-
-    private void setSimulateClick(View view, float x, float y) {
-        long downTime = SystemClock.uptimeMillis();
-        final MotionEvent downEvent = MotionEvent.obtain(downTime, downTime,
-                MotionEvent.ACTION_DOWN, x, y, 0);
-        downTime += 1000;
-        final MotionEvent upEvent = MotionEvent.obtain(downTime, downTime,
-                MotionEvent.ACTION_UP, x, y, 0);
-        view.onTouchEvent(downEvent);
-        view.onTouchEvent(upEvent);
-        downEvent.recycle();
-        upEvent.recycle();
     }
 }
